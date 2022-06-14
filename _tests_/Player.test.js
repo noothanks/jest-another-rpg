2da2be7b40a55f6c3ab93fee6b00db98f82f2c15
noothanks <coreyfuller727@yahoo.com>
2022-06-14 15:32:16 -0400

    adding player tests and constructor functions

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -19,3 +19,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test('gets player stats as an object', () => {
+    const player = new Player('Doug');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory form player or returns false', () => {
+    const player = new Player('Doug');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
